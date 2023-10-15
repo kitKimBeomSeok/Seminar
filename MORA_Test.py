6b744856caeee48e34d07785ed3b0cd629085039
Dori <qarkchanjin@gmail.com>
2023-10-15 18:47:16 +0900

    백오프 타이머 Standard 시뮬레이션 구현 중... & 교수님 MORA 코드 추가

--- MORA_Test.py
+++ MORA_Test.py
@@ -1,33 +1,38 @@
-# 네트워크 세미나 시뮬레이션 환경 구성
-# 기존 EBO의 경우 우선 순위 범위가 RA-RU의 수만큼 사용
-# 응용 EBO의 경우 STA의 수만큼 RU와 관계를 로그 관계로 풀어서 우선 순위의 범위를 동적으로 적용
-#
+# 충돌 정의 변경
+# - MORA-01: VRU(패킷) 단위로 충돌 계산
+# - MORA-02: RU 단위로 충돌 계산 (하나의 VRU에서 충돌 발생시 해당 RU 충돌로 계산)
+
 import random
 
 NUM_SIM = 1  # 시뮬레이션 반복 수
-NUM_DTI = 100000  # 1번 시뮬레이션에서 수행될 Data Transmission Interval 수
-
-# AP set
-SIFS = 16
-DIFS = 32
-NUM_RU = 8  # AP에 정해져있는 RU의 수
-PACKET_SIZE = 1000
-TF_SIZE = 89
-DATA_RATE = 1
-BA_SIZE = 32
-DTI = 32  # Data Transmission Interval 시간, 단위: us => 의미하는 바는 STA이 보내는 데이터의 길이가 각 STA마다 다르지만 정해진 시간만큼은 일정하기 때문에 DTI로 고정
+NUM_DTI = 100000  # 한 번 시뮬레이션에서 수행될 Data Transmission Interval 수
 
 # 기본 설정 파라미터 값
-RU = 8  # STA에게 할당가능한 RU의 수
-MIN_OCW = 8  # 최소 백오프 카운터
-MAX_OCW = 64  # 최대 백오프 카운터
-RETRY_BS = 10  # 백오프 스테이지 최댓값 = 충돌 실패 후 백오프타이머를 계속 시도할 때의 최대 횟수
+USER = 110  # 사용자 수
+ANTENNA = 4  # AP의 안테나 수
+RU = 8  # 사용가능한 RU
+VRU = 5  # 사용가능한 VRU
+MIN_OCW = 32  # 최소 백오프 카운터
+MAX_OCW = 1024  # 최대 백오프 카운터
+RETRY_BS = 6  # 백오프 스테이지 최댓값
+
+DATA_RATE = 1  # 데이터 전송 속도, 단위: Gbps
+PKT_SZ = 1000  # 데이터 패킷 크기, 단위: byte
+PREAMBLE_SZ = 40  # 프리앰블 크기, 단위: byte
+TF_SZ = 89  # 트리거 프레임 크기, 단위: byte
+BA_SZ = 32  # 블록 ACK 크기, 단위: byte
+BEACON = 10  # 비콘 주기, 단위: ms
+SLOT = 9  # 슬롯 크기, 단위: us
+SIFS = 16  # SIFS 시간, 단위: us
+DIFS = 18  # DIFS 시간, 단위: us
+DTI = 32  # Data Transmission Interval 시간, 단위: us
 
 # Transmission time in us
-PKT_SZ_us = (PACKET_SIZE * 8) / (DATA_RATE * 1000)  # 데이터 패킷 전송 시간, 단위: us
-TF_SZ_us = (TF_SIZE * 8) / (DATA_RATE * 1000)  # 트리거 프레임 전송 시간, 단위: us
-BA_SZ_us = (BA_SIZE * 8) / (DATA_RATE * 1000)  # 블록 ACK 전송 시간, 단위: us
-TWT_INTERVAL = DIFS + TF_SZ_us + SIFS * 2 + DTI + BA_SZ_us  # DIFS + 트리거 프레임 전송 시간 + SIFS + DTI + SIFS + Block Ack 전송 시간 => 전체 TWT 시간
+PKT_SZ_us = (PKT_SZ * 8) / (DATA_RATE * 1000)  # 데이터 패킷 전송 시간, 단위: us
+PREAMBLE_SZ_us = (PREAMBLE_SZ * 8) / (DATA_RATE * 1000)  # 프리앰블 전송 시간, 단위: us
+TF_SZ_us = (TF_SZ * 8) / (DATA_RATE * 1000)  # 트리거 프레임 전송 시간, 단위: us
+BA_SZ_us = (BA_SZ * 8) / (DATA_RATE * 1000)  # 블록 ACK 전송 시간, 단위: us
+TWT_INTERVAL = DIFS + TF_SZ_us + SIFS * 2 + DTI + BA_SZ_us
 
 # 성능 변수
 # 패킷 단위 성능
@@ -35,57 +40,58 @@ Stats_PKT_TX_Trial = 0  # 전송 시도 수
 Stats_PKT_Success = 0  # 전송 성공 수
 Stats_PKT_Collision = 0  # 충돌 발생 수
 Stats_PKT_Delay = 0  # 패킷 당 전송 시도 DTI 수
-
 # RU 단위 성능
 Stats_RU_TX_Trial = 0  # 전송 시도 수
 Stats_RU_Idle = 0  # 빈 RU 수
 Stats_RU_Success = 0  # 전송 성공 RU 수
 Stats_RU_Collision = 0  # 충돌 발생 RU 수
 
-# station 관리 목록
+# 노드 관리 목록
 stationList = []
 
 
-class Station:
+class station:
     def __init__(self):
         self.ru = 0  # 할당된 RU
+        self.vru = 0  # 할당된 VRU
         self.cw = MIN_OCW  # 초기 OCW
-        self.bo = random.randrange(0, self.cw)  # Backoff Counter
-        self.tx_status = False  # True 전송 시도, False 전송 시도 X
-        self.suc_status = False  # True 전송 성공, False 전송 실패 [충돌]
+        self.bo = random.randrange(0, self.cw)  # backoffCounter
+        self.tx_status = False  # True 전송 시도, False 전송 시도 않음
+        self.suc_status = False  # True 전송 성공, False 전송 실패(충돌)
         self.delay = 0
         self.retry = 0
-        self.data_size = 0  # 데이터 사이즈 (bytes)
+        self.data_sz = 0  # 데이터 사이즈 (bytes)
 
 
-def createSTA(USER):
+def createSTA():
     for i in range(0, USER):
-        sta = Station()
+        sta = station()
         stationList.append(sta)
 
 
-def allocationRA_RU():
+def allocateRUandVRU():
     for sta in stationList:
-        if (sta.bo <= 0):  # 백오프 타이머가 0보다 작아졌을 때
+        if (sta.bo < ANTENNA * RU):
             sta.tx_status = True  # 전송 시도
-            sta.ru = random.randrange(0, NUM_RU)  # 랜덤으로 RU 할당
+            sta.ru = sta.bo % RU
+            sta.vru = (sta.bo - sta.ru) / RU
         else:
-            sta.bo -= NUM_RU  # 백오프타이머 감소 [RU의 수만큼 점차 감소]
-            sta.tx_status = False  # 전송 시도 하지 않음.
+            sta.bo -= (ANTENNA * RU)
+            sta.tx_status = False  # 전송 시도 않음
 
 
 def setSuccess(ru):
     for sta in stationList:
-        if (sta.tx_status == True):  # 만약 전송 시도를 했다면
-            if (sta.ru != ru):  # sta에 ru 할당 적용 -> 들어간 ru가 sta 다른 RU 라면?
+        if (sta.tx_status == True):  # 전송 시도
+            if (sta.ru == ru):
                 sta.suc_status = True  # 전송 성공
 
 
 def setCollision(ru):
     for sta in stationList:
-        if (sta.tx_status == True):  # 만약 전송 시도를 했다면
-            if (sta.ru == ru):  # sta에 ru 할당 적용 -> 들어간 ru가 sta 같은 RU 라면?
-                sta.suc_status = False  # 전송 실패 [충돌]
+        if (sta.tx_status == True):  # 전송 시도
+            if (sta.ru == ru):
+                sta.suc_status = False  # 전송 실패 (충돌)
 
 
 def incRUTX():
@@ -109,22 +115,41 @@ def incRUIdle():
 
 
 def checkCollision():
-    coll_RU = []
-    for i in range(0, NUM_RU):
-        coll_RU.append(0)
+    # 충돌 확인을 위한 비트맵 초기화
+    coll_map = []  # RU 및 VRU에서 전송 시도된 노드 수를 추적하기 위한 비트맵 초기화
+    for i in range(0, RU):
+        line = []
+        for j in range(0, ANTENNA):
+            line.append(0)
+        coll_map.append(line)
+    # 여기까지가 안테나 수에 따라서 VRU까지 세팅해준 부분
+
     for sta in stationList:
-        if (sta.tx_status == True):  # 전송 시도 중인 STA만 확인
-            coll_RU[int(sta.ru)] += 1  # 선택된 RU의 인덱스 부분에 +1씩 증가 => 만약 2 이상의 값이 있다면 해당 RU는 2개 이상의 STA이 존재 => 충돌
+        if (sta.tx_status == True):  # 전송 시도 중인 노드만 확인
+            # 해당 RU 및 VRU에서 전송 시도된 노드 수 증가
+            coll_map[int(sta.ru)][int(sta.vru)] += 1
+
     # RU 단위로 충돌 여부 확인
-    for i in range(0, RU):  # STA이 할당 가능한 RU의 개수만큼 반복
+    for i in range(0, RU):
         incRUTX()  # RU 전송 시도 수 증가
         cnt_coll = 0  # RU 내에서의 충돌 수
-        if (coll_RU[i] == 1):  # 해당 인덱스의 값이 1인 경우에는 하나의 STA만 할당되었다.
-            incRUSuccess()
-        elif (coll_RU[i] <= 0):  # 해당 인덱스의 값이 0보다 작은 경우 [= 0인 경우]에는 RU가 할당되지 않았음
-            incRUIdle()
+        cnt_busy = 0  # RU 내에서의 전송 시도된 VRU 수
+        for j in range(0, ANTENNA):
+            if (coll_map[i][j] >= 1):  # 한 VRU에 전송 시도 노드 1개 이상 있음
+                cnt_busy += 1
+            if (coll_map[i][j] >= 2):  # 한 VRU에 전송 시도 노드 2개 이상 있음 - 즉, 전송 실패 (충돌)
+                cnt_coll += 1
+                break
+
+        if (cnt_coll >= 1):  # 충돌 발생한 VRU 있음 - 즉, 해당 RU에서 전송 실패
+            setCollision(i)  # 해당 RU에서 전송 실패로 표시
+            incRUCollision()  # RU 충돌 수 증가
         else:
-            incRUCollision()  # 위의 경우에 제외된 경우에는 충돌이 일어났음
+            if (cnt_busy >= 1):  # 충돌 발생한 VRU 없음 - 즉, 해당 RU에서 전송 성공
+                setSuccess(i)  # 해당 RU에서 전송 성공으로 표시
+                incRUSuccess()  # RU 성공 수 증가
+            else:
+                incRUIdle()  # 해당 RU에서 전송 시도한 노드 없음 - 해당 RU는 빈 RU임을 표시
 
 
 def addStats():
@@ -147,12 +172,14 @@ def incTrial():
     for sta in stationList:
         sta.delay += 1  # 전송 시도 수 1 증가
 
+
 def changeStaVariables():
     for sta in stationList:
         if (sta.tx_status == True):  # 전송 시도
             if (sta.suc_status == True):  # 전송 성공
-                sta.ru = 0  # 할당된 RU 초기화
-                sta.cw = MIN_OCW  # 초기 OCW -> 기존 OCW의 범위에서 재설정
+                sta.ru = 0  # 할당된 RU
+                sta.vru = 0  # 할당된 VRU
+                sta.cw = MIN_OCW  # 초기 OCW
                 sta.bo = random.randrange(0, sta.cw)  # backoffCounter
                 sta.tx_status = False  # True 전송 시도, False 전송 시도 않음
                 sta.suc_status = False  # True 전송 성공, False 전송 실패(충돌)
@@ -161,6 +188,7 @@ def changeStaVariables():
                 sta.data_sz = 0  # 데이터 사이즈 (bytes)
             else:  # 전송 실패 (충돌)
                 sta.ru = 0  # 할당된 RU
+                sta.vru = 0  # 할당된 VRU
                 sta.retry += 1
                 if (sta.retry >= RETRY_BS):  # 해당 패킷 폐기 및 변수 값 초기화
                     sta.cw = MIN_OCW  # 초기 OCW
@@ -185,7 +213,7 @@ def print_Performance():
     print("지연 : ", Stats_PKT_Delay / Stats_PKT_Success)
 
     # Transmission time in us
-    print(">> 통신 속도 : ", (Stats_PKT_Success * PACKET_SIZE * 8) / (NUM_SIM * NUM_DTI * TWT_INTERVAL))  # 단위: Mbps
+    print(">> 통신 속도 : ", (Stats_PKT_Success * PKT_SZ * 8) / (NUM_SIM * NUM_DTI * TWT_INTERVAL))  # 단위: Mbps
     print(">> 지연 : ", (Stats_PKT_Delay / Stats_PKT_Success) * TWT_INTERVAL)  # 단위: us
 
     # print(TWT_INTERVAL)
@@ -200,28 +228,11 @@ def print_Performance():
     print(">> 충돌율 : ", (Stats_RU_Collision / Stats_RU_TX_Trial) * 100)
 
 
-# def main():
-#     USER_MAX = 100
-#     for i in range(1, USER_MAX) :
-#         for k in range(0, NUM_SIM) : #시뮬레이션 횟수
-#             stationList.clear() # stationlist 초기화
-#             createSTA(i) #User의 수가 1일 때부터 100일 때까지 반복
-#         for j in range(0, NUM_DTI):
-#             incTrial()
-#             allocationRA_RU()
-#             checkCollision()
-#             addStats()
-#             changeStaVariables()
-#
-#     print_Performance()
-#
-# main()
-
 def main():
     for i in range(0, NUM_SIM):
         # 시뮬레이션 반복할 때마다 모든 노드 삭제 후 재 생성
         stationList.clear()  # 모든 노드 삭제
-        createSTA(100)  # 노드 생성
+        createSTA()  # 노드 생성
 
         for j in range(0, NUM_DTI):
             # k = 0
@@ -230,7 +241,7 @@ def main():
             #    k += 1
 
             incTrial()
-            allocationRA_RU()
+            allocateRUandVRU()
             checkCollision()
             addStats()
             changeStaVariables()
